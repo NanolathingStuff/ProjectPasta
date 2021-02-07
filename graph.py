61afc44fae12ad0c4b2cdf2cf83c7dad7dd80b6e
NanolathingStuff <silva.edoardo97@gmail.com>
2021-02-07 18:42:17 +0100

    test import

--- graph.py
+++ graph.py
@@ -1,9 +1,24 @@
-import pandas as pd
+import os
+
+#packages = os.system('pip freeze') #list')
+#print(type(packages))
+try:        
+    import pandas as pd
+except ImportError as e: #if 'pandas' not in packages:
+    os.system('pip install pandas')
+    import pandas as pd
+
+try:        
+    import xlrd
+except ImportError as e: #if 'xlrd' not in packages:
+    os.system('pip install xlrd')
+
 
 def main():
-    print("Hello World!")
-    df = pd.read_csv('ProjectPasta.csv')
-    print(df.to_string()) 
+    #print("Installing packages")
+    df1 = pd.read_excel('ProjectPasta.xls','Foglio1')
+    df2 = pd.read_excel('ProjectPasta.xls','Foglio2')
+    print(df1.to_string(), df2.to_string()) 
 
 
 if __name__ == "__main__":
